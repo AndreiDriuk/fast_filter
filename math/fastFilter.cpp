3b3bd92c49cc9a407b3dd449db02526a9deeebe4
Andrei d <ad@example.com>
2025-12-28 16:17:21 +0300

    Add decimation witout rest

--- math/fastFilter.cpp
+++ math/fastFilter.cpp
@@ -6,8 +6,38 @@
 /*
 
 */
+void filter(float* complex_data, uint32_t N1, float *complexFilter, uint32_t N2, float * dataOut, float* rest){
+    float data[4];
+    __m256 temp = _mm256_setzero_ps();
+    __m256 sum = _mm256_setzero_ps();
+    __m256 data_vec  = _mm256_setzero_ps();
+    __m256 filter_vec  = _mm256_setzero_ps();
+    for(int i = 0; i<(N1-N2); i+=2){
+        sum = _mm256_setzero_ps();
+        for(int j = 0; j<N2/8; ++j){
+            data_vec = _mm256_loadu_ps(&complex_data[i+8*j]);
+            filter_vec = _mm256_loadu_ps(&complexFilter[j*8]);
+            temp =_mm256_mul_ps(data_vec, filter_vec);
+            sum = _mm256_add_ps(sum, temp);
+                    //[s1-4, i1-4, s1-4, i1-4, s1-4, i1-4, s1-4, i1-4]
+        }
+        sum = _mm256_shuffle_ps(sum, sum,_MM_SHUFFLE(3, 1, 2, 0)); //[s1,s2, i1,i2, s3,s4, i3,i4]
+        sum = _mm256_hadd_ps(sum, sum);                            //[s12, i12, s34, i34, s12, i12, s34, i34]
+        temp = _mm256_permute_ps(sum,_MM_SHUFFLE(3, 1, 4, 0)); //[s12, s34, i12, i34, s12, s34, i12, i34]
+        
+        __m256i perm_mask = _mm256_setr_epi32(0,4,1,5,4,5,6,7);
+        sum = _mm256_permutevar8x32_ps(sum, perm_mask);
 
-void decimate(float* complex_data, uint32_t N1, float *complexFilter, uint32_t N2, float * dataOut, float *rest, uint32_t q){
+        sum = _mm256_hadd_ps(sum, sum);        
+         __m128 low = _mm256_castps256_ps128(sum);
+        _mm_storeu_ps(data, low);
+        dataOut[i] = data[0];
+        dataOut[i+1] = data[1];      
+    }
+    memcpy(rest, &complex_data[N1-N2], N2*4);
+}
+
+uint32_t decimate(float* complex_data, uint32_t N1, float *complexFilter, uint32_t N2, float * dataOut, float *rest, uint32_t q){
     float data[4];
     int k = 0;
     size_t N = (N1-N2)/q;
@@ -40,22 +70,25 @@ void decimate(float* complex_data, uint32_t N1, float *complexFilter, uint32_t N
         k+=2;      
     }
     memcpy(rest, &complex_data[N1-(N2-2*q)], (N2-2*q)*4);
+    return (N1-((N1-N2)/q)*q);// отстаток фильтра; 
 }
 
-void filter(float* complex_data, uint32_t N1, float *complexFilter, uint32_t N2, float * dataOut, float* rest){
+
+void decimate(float* complex_data, uint32_t N1, float *complexFilter, uint32_t N2, float * dataOut, uint32_t q){
+    int k = 0;
     float data[4];
     __m256 temp = _mm256_setzero_ps();
     __m256 sum = _mm256_setzero_ps();
     __m256 data_vec  = _mm256_setzero_ps();
     __m256 filter_vec  = _mm256_setzero_ps();
-    for(int i = 0; i<(N1-N2); i+=2){
+
+    for(int i = 0; i<(N1-N2); i+=2*q){
         sum = _mm256_setzero_ps();
         for(int j = 0; j<N2/8; ++j){
             data_vec = _mm256_loadu_ps(&complex_data[i+8*j]);
             filter_vec = _mm256_loadu_ps(&complexFilter[j*8]);
             temp =_mm256_mul_ps(data_vec, filter_vec);
             sum = _mm256_add_ps(sum, temp);
-                    //[s1-4, i1-4, s1-4, i1-4, s1-4, i1-4, s1-4, i1-4]
         }
         sum = _mm256_shuffle_ps(sum, sum,_MM_SHUFFLE(3, 1, 2, 0)); //[s1,s2, i1,i2, s3,s4, i3,i4]
         sum = _mm256_hadd_ps(sum, sum);                            //[s12, i12, s34, i34, s12, i12, s34, i34]
@@ -67,8 +100,8 @@ void filter(float* complex_data, uint32_t N1, float *complexFilter, uint32_t N2,
         sum = _mm256_hadd_ps(sum, sum);        
          __m128 low = _mm256_castps256_ps128(sum);
         _mm_storeu_ps(data, low);
-        dataOut[i] = data[0];
-        dataOut[i+1] = data[1];      
+        dataOut[k] = data[0];
+        dataOut[k+1] = data[1];
+        k+=2;      
     }
-    memcpy(rest, &complex_data[N1-N2], N2*4);
 }
--- math/fastFilter.h
+++ math/fastFilter.h
@@ -3,4 +3,7 @@
 // Function for fast Filtering using intinsics
 
 void filter(float* complex_data, uint32_t N1, float *complexFilter, uint32_t N2, float * dataOut, float *rest);
-void decimate(float* complex_data, uint32_t N1, float *complexFilter, uint32_t N2, float * dataOut, float *rest, uint32_t q);
+
+uint32_t decimate(float* complex_data, uint32_t N1, float *complexFilter, uint32_t N2, float * dataOut, float *rest, uint32_t q);
+
+void decimate(float* complex_data, uint32_t N1, float *complexFilter, uint32_t N2, float * dataOut, uint32_t q);
